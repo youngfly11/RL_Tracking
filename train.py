d144950a6befdda5907325cf6d11ef8f3de63024
liuyf3@shanghaitech.edu.cn <liuyf3@shanghaitech.edu.cn>
2018-01-06 22:21:50 +0800

    fix bug

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import numpy as np
 from torch.distributions import Bernoulli
 import math
 import utils
-
+from utils.misc import AverageMeter
 
 BATCH_SIZE = 64
 LEARNING_RATE = 0.01
@@ -20,39 +20,38 @@ class Trainer:
 
     def __init__(self, state_dim=256, action_space=2):
         """
-        :param state_dim: Dimensions of state (int)
-        :param action_dim: Dimension of action (int)
-        :param ram: replay memory buffer object
-        :return:
+        Args:
+            - state_dim: Dimensions of state (int)
+            - action_dim: Dimension of action (int)
         """
         self.state_dim = state_dim
-        self.action_dim = action_space
+        self.action_space = action_space
         self.model = TrackModel(pretrained=True).cuda()
 
-        self.optimizer = torch.optim.Adam([{'params': self.model.feature_extractor.parameters(), 'lr': 0.1*LEARNING_RATE},
-                                                 {'params': self.model.rnn.parameters()},
-                                                 {'params': self.model.actor.parameters()},
-                                                 {'params': self.model.critic.parameters()}],
-                                                lr=LEARNING_RATE)
+        self.optimizer = torch.optim.Adam([{'params': self.model.feature_extractor.parameters(), 'lr': 0.5*LEARNING_RATE},
+                                           {'params': self.model.rnn.parameters()},
+                                           {'params': self.model.actor.parameters()},
+                                           {'params': self.model.critic.parameters()}],
+                                           lr=LEARNING_RATE)
         self.loss1 = torch.nn.NLLLoss().cuda()
         self.loss2 = torch.nn.MSELoss().cuda()
 
-
     def get_exploration_action(self, img, hidden_prev):
         """
-        Gets the action from the target actor network.
+        Play mode to get actions
         Args:
         ----
         - img: input is the single image(1*3*height*weight)
-        - action, sample from the probability. p0 = action[0], p1 = action[1]
+        - action, sample from the probability. p0 = action[0], p1 = action[1], 1*2
         """
 
         action, hidden_pres = self.model.forward(img, action=None, is_play=True, hidden_prev=hidden_prev)
-        action = action.detach()
+        action = action.detach() # stop gradients from actions
         m = Bernoulli(action[1])
         action_final = m.sample()  # cuda tensor
-        hidden = (Variable(hidden_pres[0], Variable(hidden_pres[1])))
-        return action_final, hidden
+        action = action_final.data.cpu().numpy()[0]  # change cuda tensor to numpy
+        hidden = (Variable(hidden_pres[0].data, Variable(hidden_pres[1].data)))
+        return action, hidden
 
     def optimize(self, train_loader, hidden_pres, is_train=True):
         """
@@ -62,15 +61,17 @@ class Trainer:
         action: cuda tensor format.list
         """
 
-        # TODO
-        # the first frame need to deal
-        ## from the outside
-
+        loss_avg = AverageMeter()
+        loss1_avg = AverageMeter()
+        loss2_avg = AverageMeter()
 
         for idx, sample in enumerate(train_loader):
 
             imgs, actions, rewards = sample['images'], sample['actions'], sample['rewards']
-            imgs = Variable(imgs).cuda()
+            if is_train:
+                imgs = Variable(imgs).cuda()
+            else:
+                imgs = Variable(imgs, volatile=True).cuda()
             actions = Variable(actions).cuda()
             rewards = Variable(rewards).cuda()
 
@@ -87,7 +88,14 @@ class Trainer:
                 loss.backward()
                 self.optimizer.step()
 
-        return loss.data[0], loss1.data[0], loss2.data[0]
+            loss = loss.data.cpu().numpy()[0]
+            loss1 = loss1.data.cpu().numpy()[0]
+            loss2 = loss2.data.cpu().numpy()[0]
+            loss_avg.update(loss)
+            loss1_avg.update(loss1)
+            loss2_avg.update(loss2)
+
+        return loss_avg.avg, loss1_avg.avg, loss2_avg.avg
 
     def reward_to_go(self, rewards):
         running_add = 0
@@ -104,27 +112,3 @@ class Trainer:
         # rewards -= mean
         # rewards /= std
         return rewards
-
-
-    def save_models(self, episode_count):
-        """
-        saves the target actor and critic models
-        :param episode_count: the count of episodes iterated
-        :return:
-        """
-        torch.save(self.target_actor.state_dict(), './Models/' + str(episode_count) + '_actor.pt')
-        torch.save(self.target_critic.state_dict(), './Models/' + str(episode_count) + '_critic.pt')
-        print 'Models saved successfully'
-
-
-    def load_models(self, episode):
-        """
-        loads the target actor and critic models, and copies them onto actor and critic models
-        :param episode: the count of episodes iterated (used to find the file name)
-        :return:
-        """
-        self.actor.load_state_dict(torch.load('./Models/' + str(episode) + '_actor.pt'))
-        self.critic.load_state_dict(torch.load('./Models/' + str(episode) + '_critic.pt'))
-        utils.hard_update(self.target_actor, self.actor)
-        utils.hard_update(self.target_critic, self.critic)
-        print 'Models loaded succesfully'
